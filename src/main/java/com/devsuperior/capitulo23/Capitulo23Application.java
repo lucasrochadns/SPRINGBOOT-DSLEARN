69274e45eb27cafba28773cb1a5fdfc67237e444
Lucas Rocha <lucas.rocha-dev@outlook.com>
2023-05-28 07:54:56 -0300

    Criação do Projeto

---
+++ src/main/java/com/devsuperior/capitulo23/Capitulo23Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.capitulo23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capitulo23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Capitulo23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/capitulo23/Capitulo23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.capitulo23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Capitulo23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
