d78b976bca4bc6f43389d05dd7d58c4a6d7fbcec
Lucas Rocha <lucas.rocha-dev@outlook.com>
2023-05-28 13:41:01 -0300

    Entities

---
+++ src/main/java/com/devsuperior/capitulo23/domain/Order.java
@@ -0,0 +1,4 @@
+package com.devsuperior.capitulo23.domain;
+
+public class Order {
+}
---
+++ src/main/java/com/devsuperior/capitulo23/domain/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.devsuperior.capitulo23.domain.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED;
+
+}
